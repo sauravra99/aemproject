32f3becbca3ec7ea7e450f617e0a394d0ce04da2
Sauravra99 <68020013+sauravra99@users.noreply.github.com>
2026-02-13 09:34:35 +0530

    Adding query builder servlet

---
+++ core/src/main/java/com/example/core/servlets/QueryBuilderServlet.java
@@ -0,0 +1,88 @@
+package com.example.core.servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jcr.Session;
+import javax.jcr.RepositoryException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.result.SearchResult;
+import com.day.cq.search.result.Hit;
+
+@Component(service = Servlet.class)
+@SlingServletPaths("/bin/querybuilder-test")
+public class QueryBuilderServlet extends SlingAllMethodsServlet {
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request,
+                         SlingHttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+
+        ResourceResolver resourceResolver = request.getResourceResolver();
+
+        // Adapt QueryBuilder
+        QueryBuilder queryBuilder =
+                resourceResolver.adaptTo(QueryBuilder.class);
+
+        // Adapt Session
+        Session session = resourceResolver.adaptTo(Session.class);
+
+        // Predicate Map
+        Map<String, String> map = new HashMap<>();
+        map.put("path", "/content/mysite/us/en/home");
+        map.put("type", "cq:Page");
+        map.put("p.limit", "-1");
+
+        // Create Query
+        Query query = queryBuilder.createQuery(
+                PredicateGroup.create(map),
+                session
+        );
+
+        SearchResult result = query.getResult();
+
+        StringBuilder json = new StringBuilder();
+        json.append("{ \"pages\" : [");
+
+        boolean first = true;
+
+        try {
+            for (Hit hit : result.getHits()) {
+
+                if (!first) {
+                    json.append(",");
+                }
+
+                json.append("\"")
+                    .append(hit.getPath())
+                    .append("\"");
+
+                first = false;
+            }
+
+        } catch (RepositoryException e) {
+            response.getWriter().write("{\"error\":\"Repository error\"}");
+            return;
+        }
+
+        json.append("]}");
+
+        response.getWriter().write(json.toString());
+    }
+}
